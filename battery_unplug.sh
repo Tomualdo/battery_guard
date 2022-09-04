7da883f0e6764f934c45d775014afb71b7982bc5
Tomualdo <tomas.krajcoviech@gmail.com>
2022-09-04 17:30:36 +0200

    adjusted for other OS

--- battery_unplug.sh
+++ battery_unplug.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+
+FIRST_NOTIFY=1
+BAT=$(upower -e | grep -P -o 'BAT\d' | sed -n 1p)
 while true
 do
-  on_ac_power
+  #on_ac_power
   CHARGING=$(echo $?)
-  battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-   if [ $battery_level -ge 80 ] && [ $CHARGING -eq 0 ]; then
-      notify-send "Battery Full - Unplug the charger !!!" "Level: ${battery_level}%"
+  CHARGING=$(cat /sys/class/power_supply/$BAT/status)
+  if [ $CHARGING == "Charging" ]; then
+	  CHARGING=1
+  else
+	  CHARGING=0
+  fi
+
+  #battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+  battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_$BAT | grep -P -o '[0-9]+(?=%)' | sed -n 1p)
+  
+  # test notify-send
+  if [ $FIRST_NOTIFY -eq 1 ]; then
+	  echo "Battery guard started.."
+	  notify-send "Battery guard started..."
+	  NOTIFY_ENABLED=$(echo $?)
+	  FIRST_NOTIFY=0
+  fi
+
+   if [ $battery_level -ge 80 ] && [ $CHARGING -eq 1 ]; then
+	   if [ $NOTIFY_ENABLED -eq 0 ];then
+	      notify-send "Battery Full - Unplug the charger !!!" "Level: ${battery_level}%"
+	   fi
       paplay /usr/share/sounds/freedesktop/stereo/bell.oga
-    elif [ $battery_level -le 28 ] && [ $CHARGING -ne 0 ]; then
-      notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
+    elif [ $battery_level -le 56 ] && [ $CHARGING -ne 1 ]; then
+	   if [ $NOTIFY_ENABLED -eq 0 ];then
+      		notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
+	   fi
       paplay /usr/share/sounds/freedesktop/stereo/bell.oga
   fi
  sleep 20
